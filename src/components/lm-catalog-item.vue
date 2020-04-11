f88b25148c39934752a2c1564828873323b09eb8
lookrecius <bragin.boris@ya.ru>
2020-04-12 00:39:28 +0300

    Added: style to lm-catalog-item

--- src/components/lm-catalog-item.vue
+++ src/components/lm-catalog-item.vue
@@ -17,6 +17,11 @@
     }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+ .lm-catalog-item {
+     flex-basis: 25%;
+     box-shadow: 0 0 8px 0 #e0e0e0;
+     padding: $padding*2;
+     margin-bottom: $margin*2;
+ }
 </style>
