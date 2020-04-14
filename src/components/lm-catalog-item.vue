5dbd2331f176cc423ecfd45647c7fd5896a52cd0
lookrecius <bragin.boris@ya.ru>
2020-04-15 01:29:50 +0300

    Fixed: lm-catalog-item

--- src/components/lm-catalog-item.vue
+++ src/components/lm-catalog-item.vue
@@ -1,19 +1,35 @@
 <template>
     <div class="lm-catalog-item">
-        <p>Item1</p>
-        <p>Price</p>
-        <button>Add to cart</button>
+        <img class="lm-catalog-item__image"
+             v-bind:src=" require(`../assets/images/${productData.image}`)" alt="">
+
+        <p class="lm-catalog-item__name">{{productData.name}}</p>
+        <p class="lm-catalog-item__price">{{productData.price.toFixed()}} ₽</p>
+        <button class="lm-catalog-item__add-to-cart-btn btn"
+                @click="sendDataToParent">Add to cart</button>
     </div>
 </template>
 
 <script>
     export default {
         name: "lm-catalog-item",
-        props: {},
+        props: {
+            productData: {
+                type: Object,
+                default() {
+                    return {}
+                }
+            }
+        },
         data() {
             return {}
         },
         computed: {},
+        methods: {
+            sendDataToParent() {
+                this.$emit('sendArticle', this.productData.article)
+            }
+        }
     }
 </script>
 
@@ -23,5 +39,8 @@
      box-shadow: 0 0 8px 0 #e0e0e0;
      padding: $padding*2;
      margin-bottom: $margin*2;
+     &__image {
+         width: 150px;
+     }
  }
 </style>
