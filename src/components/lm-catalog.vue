bbc184cd643b6780ed169658a96be748d7f6eb22
lookrecius <bragin.boris@ya.ru>
2020-04-15 01:30:12 +0300

    Fixed: lm-catalog

--- src/components/lm-catalog.vue
+++ src/components/lm-catalog.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="lm-catalog">
         <h1>Каталог</h1>
-        <lm-catalog-item />
-        <lm-catalog-item />
-        <lm-catalog-item />
-        <lm-catalog-item />
-        <lm-catalog-item />
+        <div class="lm-catalog__list">
+        <lm-catalog-item
+                v-for="product in products"
+                :key="product.article"
+                v-bind:product-data="product"
+                @sendArticle="reciveArticle"
+        />
+        </div>
     </div>
 </template>
 
@@ -18,12 +21,107 @@ import lmCatalogItem from "./lm-catalog-item"
         },
         props: {},
         data() {
-            return {}
+            return {
+                products: [
+                {
+                    image: "1.jpg",
+                    name: "T-shirt 1",
+                    price: 2100.234234234,
+                    article: "T1",
+                    available: true,
+                    category: "Мужские"
+                },
+                {
+                    image: "2.jpg",
+                    name: "T-shirt 2",
+                    price: 3150.12312412,
+                    article: "T2",
+                    available: true,
+                    category: "Женские"
+                },
+                {
+                    image: "3.jpg",
+                    name: "T-shirt 3",
+                    price: 4200.51524,
+                    article: "T3",
+                    available: false,
+                    category: "Женские"
+                },
+                {
+                    image: "4.jpg",
+                    name: "T-shirt 4",
+                    price: 5300.1245512,
+                    article: "T4",
+                    available: true,
+                    category: "Мужские"
+                },
+                {
+                    image: "5.jpg",
+                    name: "T-shirt 5",
+                    price: 6500.3522314,
+                    article: "T5",
+                    available: false,
+                    category: "Женские"
+                },
+                {
+                    image: "6.jpeg",
+                    name: "T-shirt 6",
+                    price: 8700.4124123,
+                    article: "T6",
+                    available: true,
+                    category: "Женские"
+                }
+            ]
+            }
         },
         computed: {},
+        methods:{
+            reciveArticle(data) {
+                return {data}
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style lang="scss">
+    .lm-catalog {
 
+
+        &__list {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            align-items: center;
+        }
+        &__link_to_cart {
+            position: fixed;
+            top: 80px;
+            right: 10px;
+            padding: $padding*2;
+            border: solid 1px #aeaeae;
+            background: #ffffff;
+        }
+    }
+    .filters {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .range-slider {
+        width: 200px;
+        margin: auto 16px;
+        text-align: center;
+        position: relative;
+    }
+    .range-slider svg, .range-slider input[type=range] {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+    }
+    input[type=range]::-webkit-slider-thumb {
+        z-index: 2;
+        position: relative;
+        top: 2px;
+        margin-top: -7px;
+    }
 </style>
