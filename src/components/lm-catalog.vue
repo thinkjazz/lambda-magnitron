3b7752a16699dcca3f9347682b64ea1d0b8a01c7
lookrecius <bragin.boris@ya.ru>
2020-04-16 01:01:58 +0300

    Added: mounted() str 40

--- src/components/lm-catalog.vue
+++ src/components/lm-catalog.vue
@@ -3,8 +3,8 @@
         <h1>Каталог</h1>
         <div class="lm-catalog__list">
         <lm-catalog-item
-                v-for="product in products"
-                :key="product.article"
+                v-for="product in this.$store.state.products"
+                :key="product.id"
                 v-bind:product-data="product"
                 @sendArticle="reciveArticle"
         />
@@ -13,7 +13,8 @@
 </template>
 
 <script>
-import lmCatalogItem from "./lm-catalog-item"
+import lmCatalogItem from "./lm-catalog-item";
+import {mapActions} from "vuex";
     export default {
         name: "lm-catalog",
         components: {
@@ -22,63 +23,22 @@ import lmCatalogItem from "./lm-catalog-item"
         props: {},
         data() {
             return {
-                products: [
-                {
-                    image: "1.jpg",
-                    name: "T-shirt 1",
-                    price: 2100.234234234,
-                    article: "T1",
-                    available: true,
-                    category: "Мужские"
-                },
-                {
-                    image: "2.jpg",
-                    name: "T-shirt 2",
-                    price: 3150.12312412,
-                    article: "T2",
-                    available: true,
-                    category: "Женские"
-                },
-                {
-                    image: "3.jpg",
-                    name: "T-shirt 3",
-                    price: 4200.51524,
-                    article: "T3",
-                    available: false,
-                    category: "Женские"
-                },
-                {
-                    image: "4.jpg",
-                    name: "T-shirt 4",
-                    price: 5300.1245512,
-                    article: "T4",
-                    available: true,
-                    category: "Мужские"
-                },
-                {
-                    image: "5.jpg",
-                    name: "T-shirt 5",
-                    price: 6500.3522314,
-                    article: "T5",
-                    available: false,
-                    category: "Женские"
-                },
-                {
-                    image: "6.jpeg",
-                    name: "T-shirt 6",
-                    price: 8700.4124123,
-                    article: "T6",
-                    available: true,
-                    category: "Женские"
-                }
-            ]
+
             }
         },
+
         computed: {},
         methods:{
+            ...mapActions([
+                'GET_PRODUCT_FROM_API'
+            ]),
             reciveArticle(data) {
                 return {data}
-            }
+            },
+
+        },
+        mounted() {
+            this.GET_PRODUCT_FROM_API()
         }
     }
 </script>
